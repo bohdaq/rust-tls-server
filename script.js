a7e50813d735dc87edf5ee8e90a5627816ee4e5e
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-18 01:30:06 +0200

    wip

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.querySelectorAll('.location').forEach(
     (element) => {
         element.innerHTML = location.href;
-        element.href = location.href;
     }
 )
 
@@ -9,4 +8,3 @@ let crt_location = location.href + 'certificate.crt';
 let crt_element = document.querySelector('.location-crt');
 
 crt_element.innerHTML = crt_location;
-crt_element.href = crt_location;
