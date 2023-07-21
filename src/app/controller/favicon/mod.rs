af5629a832d1f4b0ba0ad425d6eaed554d2b2452
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-07-21 16:41:14 +0300

    15.1.0 RC5

--- src/app/controller/favicon/mod.rs
+++ src/app/controller/favicon/mod.rs
@@ -1,19 +1,19 @@
 use file_ext::FileExt;
+use rust_web_server::controller::Controller;
 use rust_web_server::mime_type::MimeType;
 use rust_web_server::range::Range;
 use rust_web_server::request::Request;
 use rust_web_server::response::{Response, STATUS_CODE_REASON_PHRASE};
+use rust_web_server::server::ConnectionInfo;
 
 pub struct FaviconController;
 
-impl FaviconController {
-    pub const STYLE_FILEPATH: &'static str = "favicon.svg";
-
-    pub fn is_matching_request(request: &Request) -> bool {
+impl Controller for FaviconController {
+    fn is_matching(request: &Request, _connection: &ConnectionInfo) -> bool {
         request.request_uri == "/favicon.svg"
     }
 
-    pub fn process_request(_request: &Request, mut response: Response) -> Response {
+    fn process(_request: &Request, mut response: Response, _connection: &ConnectionInfo) -> Response {
         response.status_code = *STATUS_CODE_REASON_PHRASE.n200_ok.status_code;
         response.reason_phrase = STATUS_CODE_REASON_PHRASE.n200_ok.reason_phrase.to_string();
 
@@ -53,4 +53,8 @@ impl FaviconController {
 
         response
     }
+}
+
+impl FaviconController {
+    pub const STYLE_FILEPATH: &'static str = "favicon.svg";
 }
